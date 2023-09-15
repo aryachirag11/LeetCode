8cabf8c0007620db67311ba44bb050b3d1a35b2c
aryachirag11 <aryachirag11@gmail.com>
2023-09-15 23:18:58 +0530

    half challenge done without notes

---
+++ September Challenge/1282_Group the People Given the Group Size They Belong To.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<vector<int>> groupThePeople(vector<int> &groupSizes)
+  {
+    vector<vector<int>> ans;
+    unordered_map<int, vector<int>> groupSizeToIndices;
+
+    for (int i = 0; i < groupSizes.size(); ++i)
+      groupSizeToIndices[groupSizes[i]].push_back(i);
+
+    for (const auto &[groupSize, indices] : groupSizeToIndices)
+    {
+      vector<int> groupIndices;
+      for (const int index : indices)
+      {
+        groupIndices.push_back(index);
+        if (groupIndices.size() == groupSize)
+        {
+          ans.push_back(groupIndices);
+          groupIndices.clear();
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+
+int main()
+{
+  return 0;
+}
