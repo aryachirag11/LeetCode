546a90e9796e2a7ccc153f687ad429901821b0f9
aryachirag11 <aryachirag11@gmail.com>
2023-09-21 22:49:14 +0530

    updating upto 21 SEP

---
+++ September Challenge/287_Find the Duplicate Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+class Solution
+{
+public:
+  int findDuplicate(vector<int> &nums)
+  {
+    while (nums[0] != nums[nums[0]])
+    {
+      swap(nums[0], nums[nums[0]]);
+    }
+    return nums[0];
+  }
+};
+
+using namespace std;
+int main()
+{
+  return 0;
+}
